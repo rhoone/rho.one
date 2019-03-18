25c06be32df1ec2c5221ac1d7972adf4cdeed9f5
Zhao vistart <i@vistart.me>
2019-03-18 17:18:37 +0800

    Supplement cnzz.

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,8 @@ return [
     'adminEmail' => 'i@vistart.me',
     'supportEmail' => 'i@vistart.me',
     'user.passwordResetTokenExpire' => 3600,
+    'cnzz' => [
+        'code' => '<script type="text/javascript" src="https://s96.cnzz.com/z_stat.php?id=1275753753&web_id=1275753753"></script>',
+        'ajaxTrackPageView' => true,
+    ],
 ];
--- rho.one/config/params.php
+++ rho.one/config/params.php
@@ -1,4 +1,8 @@
 <?php
 return [
     'adminEmail' => 'i@vistart.me',
+    'cnzz' => [
+        'code' => '<script type="text/javascript" src="https://s96.cnzz.com/z_stat.php?id=1275753753&web_id=1275753753"></script>',
+        'ajaxTrackPageView' => true,
+    ],
 ];
