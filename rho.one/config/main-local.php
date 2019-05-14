777d9e7d073029f75446573c9051b446a4f98fcc
Zhao vistart <i@vistart.me>
2019-05-14 16:18:34 +0800

    Add MongoDbPanel for debug module.

--- rho.one/config/main-local.php
+++ rho.one/config/main-local.php
@@ -12,6 +12,14 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*'],
+        'panels' => [
+            'elasticsearch' => 'yii\elasticsearch\DebugPanel',
+            'mongodb' => [
+                'class' => 'yii\mongodb\debug\MongoDbPanel',
+                // 'db' => 'mongodb', // MongoDB component ID, defaults to `db`. Uncomment and change this line, if you registered MongoDB component with a different ID.
+            ],
+        ],
     ];
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
